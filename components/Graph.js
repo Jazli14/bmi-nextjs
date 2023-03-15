5485aa49f64d9948f797da3c658bdf9f31ce959d
Jazli14 <95947726+Jazli14@users.noreply.github.com>
2023-03-14 22:53:03 -0400

    Update Graph.js
    clean up

--- components/Graph.js
+++ components/Graph.js
@@ -59,41 +59,7 @@ export default function Graph() {
         .call(yAxis);
   });
 
-  // const width = window.innerWidth - 150
-  // const height = window.innerHeight - 90
-
-  // useEffect(() => {
-  // const dimensions = {
-  //     width: window.innerWidth - 150,
-  //     height: window.innerHeight - 90,
-  //     margin: {top: 90, left: 90, bottom: 90, right: 90}
-  // }
-  // const svg = d3.select(HeatmapChart.current)
-  //                 .append("svg")
-  //                 .style('background-color', 'yellow')
-  //                 .attr('width', dimensions.width)
-  //                 .attr('height', dimensions.height)
-  //                 .style('background-color', 'yellow')
-  //                 .style('margin-top', '')
-  // const xScale = d3.scaleLinear()
-  //             .domain([0, data.length])
-  //             .range([0, dimensions.width])
-  // const yScale = d3.scaleLinear()
-  //             .domain([0, dimensions.height])
-  //             .range([dimensions.height, 0])
-
-  // const xAxis = d3.axisBottom(xScale)
-  // svg.selectAll('weight')
-  //     .data(data)
-  // //     .join('path')
-  // svg.append('g')
-  //     .selectAll('text')
-  //     .data(data)
-  //     .join('text')
-  //     .text()
-
-  // })
-
+ 
   return (
     <div className={styles.container}>
       <div className={styles.header}>BMI GRAPH</div>
